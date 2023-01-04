f2c38df9c1679ddcaa67a20a3382abfade362ffc
mwendamwenda <benmwenda83@gmail.com>
2023-01-03 20:08:25 -0800

    secon last

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 console.log('amazing');
-const ageMwenda = 2037 - 1992;
-const ageTokiti =2037 - 1999;
+
+// math operators
+const now = 2037;
+const ageMwenda = now - 1992;
+const ageTokiti =now - 1999;
+
 console.log(ageMwenda,ageTokiti);
+console.log(ageMwenda * 2 ,ageMwenda /2, 2**3);
+
+//2**3 means 2 X 2 X2(exponential);
+const firstName = 'Benson';
+const secondName = 'mwenda';
+console.log(firstName +  ' ' + secondName);//we use the empty string to create space between two values
+
+// assignment operators
+let x = 10 + 5;
+x += 10; // x = x +10
+x*= 4;
+x ++;
+x --;
+console.log(x);
+
+//comparison operators
+
+console.log(ageMwenda > ageTokiti);
+console.log(ageTokiti >= 18);
+
+const isFullAge = ageTokiti >= 18;
